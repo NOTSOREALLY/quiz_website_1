cdb3c5b26a64995888f6f4e311ec4fca372b5638
NOTSOREALLY <193393@cloud.rangitoto.school.nz>
2026-06-01 14:59:39 +1200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,25 @@
+let currentq = 1;
+function showQuestion(qNum) {
+    for (let i = 1; i <= 15; i++) {
+        let qDiv = document.getElementById("q" + i);
+        if (qDiv) {
+            qDiv.style.display = "none";
+        }
+    }
+    let currentDiv = document.getElementById("q" + qNum);
+    if (currentDiv) {
+        currentDiv.style.display = "block";
+    }
 
+    if (qNum === 1) {
+        document.getElementById("prevBtn").disabled = true;
+    } else {
+        document.getElementById("prevBtn").disabled = false;
+    }
+
+    if (qNum === 15) {
+        document.getElementById("nextBtn").innerHTML = "SHOW SCORE";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "NEXT";
+    }
+}
