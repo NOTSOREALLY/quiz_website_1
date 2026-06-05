1e7f04f55f23ec8f000bcdf5bc342f92f436eb1b
NOTSOREALLY <193393@cloud.rangitoto.school.nz>
2026-06-05 13:28:18 +1200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,5 +1,13 @@
 let currentnum = 1;
 
 function next(totalquestions){
- var q1answer = document.getElementById(q + currentnum); 
+ let cq = document.getElementById("q" + currentnum); 
+ if (cq) cq.style.display = "none";
+
+currentnum++;
+
+if (currentnum <= totalquestions){
+   let nq = document.getElementById("q" + currentnum);
+   if (nq) nq.style.display = "block";
+ }
 }
