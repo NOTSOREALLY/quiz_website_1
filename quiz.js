696401868c2e96bc900ba8edd0bb746cfe45958b
NOTSOREALLY <193393@cloud.rangitoto.school.nz>
2026-06-07 10:42:03 +1200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -7,7 +7,7 @@ function next(){
 currentnum++;
 
   let nq = document.getElementById("q" + currentnum);
-  if (nq) nq.style.display = "inline-block";
+  if (nq) nq.style.display = "inline-grid";
 }
 
 function back(){
@@ -18,7 +18,7 @@ function back(){
    currentnum--;
 
    let bq = document.getElementById("q" + currentnum);
-   if (bq) bq.style.display = "inline-block";
+   if (bq) bq.style.display = "inlinegrid";
 }
 /*
 function submit(){
